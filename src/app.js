bbc1c31a6f1a184a97f634b83b7de1e1330ef53a
John <mrooze@v-hiker.cn>
2020-02-14 23:06:50 +0800

    update qtk and qtv.

--- src/app.js
+++ src/app.js
@@ -1,56 +1,94 @@
 import "@babel/polyfill";
-let keywords = process.argv[2];
+let keywords = process.argv[2] || "";
 keywords = keywords.replace(/\/|\\/gi, " ").trim();
 
 const fetch = require("node-fetch");
 const qs = require("qs");
-const uri = "https://fanyi.qq.com/api/translate";
-const data = {
-  source: "auto",
-  target: "zh",
-  sourceText: keywords,
-  essionUuid: "translate_uuid" + Date.now(),
-  qtv: "092fe6ce09a7ffd2",
-  qtk:
-    "7ln4zCbJpbn0QhpTlnmVQD1Pnp+4FBZ/5oeacMNW/vlkvgpKtYYuHX2ykxuI9ZB3p8W7cMg8zyCa3Qnp4Tyg5VOXSBvv+yCd81lmCqQl4Bkt/BG43wzkRnGFeG4PAf/zb6dT6UAgYFmPlRuYMGTrNQ=="
-};
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
-const options = {
-  method: "POST",
-  body: qs.stringify(data),
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    Host: "fanyi.qq.com",
-    "User-Agent":
-      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
-    Referer: "https://fanyi.qq.com/"
-  }
-};
-fetch(uri, options)
-  .then(res => res.json())
-  .then(res => {
-    let items = [];
-    items.push({
-      title: res?.translate?.records[0]?.sourceText,
-      subtitle: res?.translate?.records[0]?.targetText,
-      arg: "",
-      icon: {
-        path: ""
-      }
-    });
-    res?.suggest?.data.forEach(element => {
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+let qtv, qtk;
+
+function translate() {
+  const uri = "https://fanyi.qq.com/api/translate";
+  let qtv = "";
+  let qtk = "";
+  const data = {
+    source: "auto",
+    target: "zh",
+    sourceText: keywords,
+    essionUuid: "translate_uuid" + Date.now(),
+    qtv: qtv,
+    qtk: qtk
+  };
+
+  const options = {
+    method: "POST",
+    body: qs.stringify(data),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      Host: "fanyi.qq.com",
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+      Referer: "https://fanyi.qq.com/"
+    }
+  };
+  fetch(uri, options)
+    .then(res => res.json())
+    .then(res => {
+      let items = [];
       items.push({
-        title: element.word,
-        subtitle: element.suggest_translation,
+        title: res?.translate?.records[0]?.sourceText,
+        subtitle: res?.translate?.records[0]?.targetText,
         arg: "",
         icon: {
           path: ""
         }
       });
+      res?.suggest?.data.forEach(element => {
+        items.push({
+          title: element.word,
+          subtitle: element.suggest_translation,
+          arg: "",
+          icon: {
+            path: ""
+          }
+        });
+      });
+      console.log(
+        JSON.stringify({
+          items: items
+        })
+      );
     });
-    console.log(
-      JSON.stringify({
-        items: items
-      })
-    );
-  });
+}
+
+let t = db.has("t").value();
+
+if (!t || (t && Math.floor((Date.now() - t) / 1000) > 60 * 60 * 60)) {
+  fetch("https://fanyi.qq.com/", {
+    headers: {
+      "Content-Type": "text/html; charset=utf-8",
+      Host: "fanyi.qq.com",
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+      Referer: "https://fanyi.qq.com/"
+    }
+  })
+    .then(res => res.text())
+    .then(res => {
+      qtv = res.match(/"qtv=(.+?)"/i)[1];
+      qtk = res.match(/"qtk=(.+?)"/i)[1];
+      db.set("qtv", qtv).write();
+      db.set("qtk", qtk).write();
+      db.set("t", Date.now()).write();
+      translate();
+    });
+} else {
+  qtv = db.get("qtv").value();
+  qtk = db.get("qtk").value();
+  translate();
+}
